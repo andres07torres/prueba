f3f6cf49ba763d5201b2c89bffa88279acc3a06a
Andres Torres <andres07torresg@gmail.com>
2026-08-20 20:28:53 -0500

    chore: actualizacion adicional

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def root():
     <body>
         <h1>Entorno de Preview Funcionando</h1>
         <p>Esta app se despliega automaticamente en el subdominio asignado.</p>
-        <p>v7 - Nueva pantalla</p>
+        <p>v8 - Actualizacion adicional</p>
     </body>
     </html>
     """
