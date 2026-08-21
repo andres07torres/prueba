0752c71a324c1d9dbe4480989f2a06d395265603
Andres Torres <andres07torresg@gmail.com>
2026-08-20 20:01:35 -0500

    feat: prueba final exitosa

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def root():
     <body>
         <h1>Entorno de Preview Funcionando</h1>
         <p>Esta app se despliega automaticamente en el subdominio asignado.</p>
-        <p>v3 - Segundo cambio de prueba</p>
+        <p>v6 - Prueba final exitosa</p>
     </body>
     </html>
     """
